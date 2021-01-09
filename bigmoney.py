91893671ef4b93f6d9ecab89a9442945d68f9c13
ssywh97 <ssywh97@gmail.com>
2021-01-10 03:37:23 +0900

    큰수의 법칙

--- bigmoney.py
+++ bigmoney.py
@@ -1,3 +1,4 @@
+#단순한 풀이
 n, m, k = map(int, input().split()) 
 data = list(map(int, input().split())) 
 
@@ -8,14 +9,14 @@ print(data)
 result = 0
 
 while True:
-    for i in range (k):
+    for i in range (k): #k번 반복함으로써 최대 k번 반복해서 더해야하는제약조건을 지킴
         if m == 0:
             break
         result += first
         m -= 1 #한번 더했으므로 횟수 1회 차감 
     if m == 0:
         break
-    result += second
+    result += second #두번째로 큰 값을 한번 더해주고(최댓값을 위해) 다시 for반복문을 실행
     m -= 1
 
 print(result)
