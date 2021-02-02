16c7206ac8a3a02a29cac8d3fd8a5c5d03c3872f
ssywh97 <ssywh97@gmail.com>
2021-02-02 15:11:48 +0900

    한조문제(리스트 값 비교)

---
+++ hanzo.py
@@ -0,0 +1,16 @@
+N = int(input())
+S = list(map(int, input().split()))
+
+count = 0
+Max = 0
+ans = 0
+
+for i in S:
+    if i > Max:
+      Max = i
+      count = 0
+    else:
+      count += 1
+    ans = max(ans, count)
+
+print(ans)
