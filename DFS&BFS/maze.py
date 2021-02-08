6667cc07d6d0e482e6b988be8ac8ccce4a95a536
ssywh97 <ssywh97@gmail.com>
2021-02-08 17:22:26 +0900

    미로탈출(BFS활용)

---
+++ DFS&BFS/maze.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+#n, m을 공백으로 구분하여 입력받기
+n,m = map(int, input().split())
+
+#2차원 리스트의 맵 정보 입력받기
+graph = [] #빈 그래프(리스트)
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+#이동할 네 방향 정의(상, 하, 좌, 우)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+#BFS 소스코드 구현
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        #현재 위치에서 네 방향으로의 위치 확인((-1, 0), (1, 0), (0, -1), (0, 1))
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            #미로찾기 공간을 벗어난 경우 무시
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+
+            #벽인경우 무시(0)
+            if graph[nx][ny] == 0:
+                continue
+
+            #해당 노드를 처음 방문하는 경우에만 최단거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return graph[n - 1][m - 1]
+
+print(bfs(0, 0)) 
